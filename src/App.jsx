6eef296192189f01aa721c2a3f92e7b39defb09a
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-07-26 17:17:08 -0400

    Project - App - creating app basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,15 @@ import { useState } from 'react'
 
 function App() {
   return (
-    <main className="m-auto text-red-400">Welcome</main>
+    <main className="m-auto w-full max-w-2xl">
+      <header className="flex">
+        <img className="m-auto" src="/logo.svg" alt="Splitter logo" />
+      </header>
+      <section className="p-4 mt-14 bg-white rounded-xl shadow-6xl lg:flex">
+        <div className="h-64 p-4 lg:flex-1">Left container</div>
+        <div className="h-64 rounded-xl bg-primary p-4 lg:flex-1">Right container</div>
+      </section>
+    </main>
   )
 }
 
