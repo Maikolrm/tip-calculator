628efc87cd06e68a7048a001743780e4db237598
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-07-26 17:15:54 -0400

    Project - tailwind - extending theme ( colors )

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,16 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "hsl(172, 67%, 45%)",
+        "dark-cyan": "hsl(183, 100%, 15%)",
+        "dark-gr-cyan": "hsl(186, 14%, 43%)",
+        "gr-cyan": "hsl(184, 14%, 56%)",
+        "light-cyan": "hsl(185, 41%, 84%)",
+        "light-gr-cyan": "hsl(189, 41%, 97%)",
+      }
+    },
   },
   plugins: [],
 }
