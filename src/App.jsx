e45a1fc127d8c06e32e703d63c0829f35587d643
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-07-26 22:36:35 -0400

    Project - App - setting app initial state using immer

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,48 @@
 import { useState } from 'react'
+import { useImmerReducer } from "use-immer"
+
+// context
+import AppState from "./AppState"
+import AppDispatch from "./AppDispatch"
 
 // components
 import SplitterBill from "./components/SplitterBill"
 import SplitterTotal from "./components/SplitterTotal"
 
 function App() {
+  // initialState
+  const initialState =  {
+    bill: 0,
+    people: 0,
+    percent: 5,
+    amount: 0,
+    total: 0
+  }
+
+  // reducer
+  function reducer(draft, action) {
+    switch(action.type) {
+      case "":
+        break
+    }
+  }
+
+  const [state, dispatch] = useImmerReducer(reducer, initialState)
+
   return (
-    <main className="m-auto w-full max-w-4xl font-space-mono">
-      <header className="flex">
-        <img className="m-auto" src="/logo.svg" alt="Splitter logo" />
-      </header>
-      <section className="p-6 mt-14 bg-white rounded-2xl shadow-6xl lg:flex">
-        <SplitterBill />
-        <SplitterTotal />
-      </section>
-    </main>
+    <AppState.Provider value={state}>
+      <AppDispatch.Provider value={dispatch}>
+        <main className="m-auto w-full max-w-4xl font-space-mono">
+          <header className="flex">
+            <img className="m-auto" src="/logo.svg" alt="Splitter logo" />
+          </header>
+          <section className="p-6 mt-14 bg-white rounded-2xl shadow-6xl lg:flex">
+            <SplitterBill />
+            <SplitterTotal />
+          </section>
+        </main>
+      </AppDispatch.Provider>
+    </AppState.Provider>
   )
 }
 
