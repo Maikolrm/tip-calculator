36c263f9a88f5527ffecd59422c40071420b67a4
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-07-27 13:31:06 -0400

    Components - SplitterTotal - pulling component information from app state

--- src/components/SplitterTotal.jsx
+++ src/components/SplitterTotal.jsx
@@ -1,4 +1,12 @@
+import { useContext } from "react"
+
+// context
+import AppState from "../AppState"
+
 export default function SplitterTotal(props) {
+  // app state
+  const appState = useContext(AppState)
+
   return (
     <div className="mt-6 p-9 rounded-xl bg-dark-cyan lg:flex-1 lg:mt-0">
       <div className="pb-36">
@@ -7,14 +15,14 @@ export default function SplitterTotal(props) {
             <h2 className="opacity-90 font-semibold text-light-gr-cyan leading-none">Tip amount</h2>
             <p className="mt-2 text-sm text-gr-cyan leading-none">/ person</p>
           </div>
-          <h2 className="font-bold text-5xl text-primary">$4.27</h2>
+          <h2 className="font-bold text-5xl text-primary">${appState.amount.toFixed(2)}</h2>
         </div>
         <div className="flex items-center mt-8">
           <div className="flex-1">
             <h2 className="opacity-90 font-semibold text-light-gr-cyan leading-none">Total</h2>
             <p className="mt-2 text-sm text-gr-cyan leading-none">/ person</p>
           </div>
-          <h2 className="font-bold text-5xl text-primary">$32.79</h2>
+          <h2 className="font-bold text-5xl text-primary">${appState.total.toFixed(2)}</h2>
         </div>
       </div>
       <button className="block w-full bg-primary rounded-md font-bold text-dark-cyan leading-10 uppercase tracking-widest">reset</button>
