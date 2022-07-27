812de01213a28df551dbbdf25c73d6ba963e4078
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-07-26 21:17:10 -0400

    Components - SplitterBll - creating component ( TipButton )

--- src/components/SplitterBill.jsx
+++ src/components/SplitterBill.jsx
@@ -1,3 +1,12 @@
+// components
+function TipButton(props) {
+  return (
+    <button className="inline-block p-2 bg-dark-cyan rounded-md font-bold text-xl text-light-gr-cyan hover:bg-primary hover:text-dark-cyan">
+      {props.percent}%
+    </button>
+  )
+}
+
 export default function SplitterBill(props) {
   return (
     <div className="flex flex-col justify-between lg:px-6 lg:flex-1">
@@ -11,11 +20,11 @@ export default function SplitterBill(props) {
       <div className="mt-8 lg:mt-0">
         <h2 className="font-semibold text-gr-cyan leading-none">Select tip %</h2>
         <div className="grid grid-cols-2 gap-2 pt-3 sm:grid-cols-3">
-          <button className="inline-block p-2 bg-dark-cyan rounded-md font-bold text-xl text-light-gr-cyan hover:bg-primary hover:text-dark-cyan">5%</button>
-          <button className="inline-block p-2 bg-dark-cyan rounded-md font-bold text-xl text-light-gr-cyan hover:bg-primary hover:text-dark-cyan">10%</button>
-          <button className="inline-block p-2 bg-dark-cyan rounded-md font-bold text-xl text-light-gr-cyan hover:bg-primary hover:text-dark-cyan">15%</button>
-          <button className="inline-block p-2 bg-dark-cyan rounded-md font-bold text-xl text-light-gr-cyan hover:bg-primary hover:text-dark-cyan">25%</button>
-          <button className="inline-block p-2 bg-dark-cyan rounded-md font-bold text-xl text-light-gr-cyan hover:bg-primary hover:text-dark-cyan">50%</button>
+          <TipButton percent="5" />
+          <TipButton percent="10" />
+          <TipButton percent="15" />
+          <TipButton percent="25" />
+          <TipButton percent="50" />
           <button>Custom</button>
         </div>
       </div>
