c54a96948397707c323cb047ed80d490cf8aa3d8
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-08-01 23:37:34 -0400

    Project - App - addig box shadow to main container

--- src/App.jsx
+++ src/App.jsx
@@ -61,7 +61,7 @@ function App() {
           <header className="flex">
             <img className="m-auto" src="/logo.svg" alt="Splitter logo" />
           </header>
-          <section className="p-6 mt-14 bg-white rounded-2xl shadow-6xl lg:flex">
+          <section className="p-6 mt-14 bg-white rounded-2xl shadow-xl lg:flex">
             <SplitterBill />
             <SplitterTotal />
           </section>
